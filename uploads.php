4e85d6a86bb40420d41ff34708c09bee3370837c
Trofymenko Vitalii <trofymenko.vitalii@gmail.com>
2019-05-25 21:52:07 +0300

    update uploads

--- uploads.php
+++ uploads.php
@@ -1,20 +1,26 @@
 <?php
 $path = realpath('./uploads');
-$files = array_diff(scandir($path), array('.', '..'));
-
-if (!empty($files)) {
-    $filePath = $path . '/' . end($files);
-    if(file_exists($filePath)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($filePath).'"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($filePath));
-        flush();
-        readfile($filePath);
-        exit;
+$mostRecentFilePath = "";
+$mostRecentFileMTime = 0;
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::CHILD_FIRST);
+foreach ($iterator as $fileinfo) {
+    if ($fileinfo->isFile()) {
+        if ($fileinfo->getMTime() > $mostRecentFileMTime) {
+            $mostRecentFileMTime = $fileinfo->getMTime();
+            $mostRecentFilePath = $fileinfo->getPathname();
+        }
     }
+}
+if ($mostRecentFilePath) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($mostRecentFilePath).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($mostRecentFilePath));
+    flush();
+    readfile($mostRecentFilePath);
+    exit;
 }
 echo 'You haven`t loaded any file yet:)';
